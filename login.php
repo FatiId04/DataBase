011e45efd217d47ece9c55c71cd64f8a6a8b734d
FatiId04 <132940745+FatiId04@users.noreply.github.com>
2023-07-21 14:50:30 +0100

    Update login.php

--- login.php
+++ login.php
@@ -7,37 +7,45 @@ $password = $_GET['password'];
 $mysqli_query1 = "SELECT * FROM users WHERE identifiant=? ";
 $stmt1 = $conn->prepare($mysqli_query1);
 $stmt1->bind_param("s", $identifiant);
- if ($stmt1->execute()) {
-   $result1 = $stmt1->get_result();
-   if ($result1->num_rows > 0) {
 
+if ($stmt1->execute()) {
+    $stmt1->store_result();
 
-            if (isset($_GET['identifiant']) && isset($_GET['password'])) {
-                   $mysqli_query = "SELECT * FROM users WHERE identifiant=? AND password=?";
-                   $stmt = $conn->prepare($mysqli_query);
-                   $stmt->bind_param("ss", $identifiant, $password);
+    if ($stmt1->num_rows > 0) {
+        if (isset($_GET['identifiant']) && isset($_GET['password'])) {
+            $mysqli_query = "SELECT * FROM users WHERE identifiant=? AND password=?";
+            $stmt = $conn->prepare($mysqli_query);
+            $stmt->bind_param("ss", $identifiant, $password);
 
-                   if ($stmt->execute()) {
-                       $result = $stmt->get_result();
+            if ($stmt->execute()) {
+                $stmt->store_result();
 
-                       if ($result->num_rows > 0)  {
-                          while ($row = $result->fetch_assoc()) {
-                               $response['user'] = $row;
-                        }
-                        $response['error'] = "200";
-                        $response['message'] = "Bienvenue";
-					    }else {
-                              $response['user'] = (object)[];
-                              $response['error'] = "400";
-                              $response['message'] = "Mot de passe erroné";
-                        }           
-	                }
+                if ($stmt->num_rows > 0) {
+                    $stmt->bind_result($id, $mdp);
+                    while ($stmt->fetch()) {
+                        $response['user']['identifiant'] = $id;
+                        $response['user']['password'] = $mdp;
+                       
+                    }
+                    $response['error'] = "200";
+                    $response['message'] = "Bienvenue";
+                } else {
+                    $response['user'] = (object)[];
+                    $response['error'] = "400";
+                    $response['message'] = "Mot de passe erroné";
+                }
             }
+        }
     } else {
         $response['user'] = (object)[];
-        $response['error'] = "401";
+        $response['error'] = "400";
         $response['message'] = "utilisateur introuvable";
     }
+}
+
+echo json_encode($response);
+?>
+
 
 }
 
